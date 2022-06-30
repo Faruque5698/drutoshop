c1f8ef50c7b26c9f414c86afc47915334792d2e1
Md. Ashaduzzaman Faruque <Ashaduzzaman5698@gmail.com>
2022-06-30 16:52:13 +0100

    Category Api, Brand Api, Brand Product Api, subcategory Product Api, All Product Api Done

--- routes/api.php
+++ routes/api.php
@@ -21,9 +21,18 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 Route::post('register',[\App\Http\Controllers\Api\AuthController::class,'register']);
 Route::post('login',[\App\Http\Controllers\Api\AuthController::class,'login']);
 
+Route::get('brand',[\App\Http\Controllers\Api\CategoryController::class,'brand']);
+Route::get('category',[\App\Http\Controllers\Api\CategoryController::class,'category']);
+Route::get('subcat-product/{id}',[\App\Http\Controllers\Api\CategoryController::class,'subcatProduct']);
+Route::get('brand-product/{id}',[\App\Http\Controllers\Api\CategoryController::class,'brandProduct']);
+Route::get('all-products',[\App\Http\Controllers\Api\CategoryController::class,'product']);
+
 Route::group(["middleware" => ["auth:api"]], function(){
 
     Route::get("profile", [\App\Http\Controllers\Api\AuthController::class, "profile"]);
+
+
+
     Route::post("logout", [\App\Http\Controllers\Api\AuthController::class, "logout"]);
 
 
